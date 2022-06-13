b898594f04d24716d0c6664fe0115f8e7222d57e
Rodrigo Catalán López <ext_recatalanl@falabella.cl>
2022-06-13 10:39:00 -0400

    fix: navigation componente are now passing testing
    Signed-off-by: Rodrigo Catalán López <ext_recatalanl@falabella.cl>

--- src/routes/test/Navigation.test.tsx
+++ src/routes/test/Navigation.test.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
-import Navigation from '../Navigation';
-import { Provider } from 'react-redux';
-import { store } from '../../app/store';
+import React from "react";
+import { render } from "@testing-library/react";
+import Navigation from "../Navigation";
+import { Provider } from "react-redux";
+import { store } from "../../app/store";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(
     <Provider store={store}>
-      <Navigation />
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>
     </Provider>
   );
 });
